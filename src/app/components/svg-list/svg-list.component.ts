28e773b6daa781f38df1decb9088c5b9a3162075
AndrewJBateman <gomezbateman@yahoo.com>
2022-01-09 19:38:55 +0100

    feat: add icons % remove unused code

---
+++ src/app/components/svg-list/svg-list.component.ts
@@ -0,0 +1,8 @@
+import { Component } from "@angular/core";
+
+@Component({
+  selector: "app-svg-list",
+  templateUrl: "../../../../src/assets/svgs/list.svg",
+  styleUrls: ["../svg.component.css"],
+})
+export class SvgListComponent {}
