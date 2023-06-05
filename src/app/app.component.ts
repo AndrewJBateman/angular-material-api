0d0c42390f19b97d3df47019ff489ac0f2131660
AndrewJBateman <gomezbateman@yahoo.com>
2023-06-05 16:43:15 +0200

    refactor: remove excess code

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,8 +2,12 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  template: `<div class="app">
+  <app-navbar></app-navbar>
+  <div class="main">
+    <router-outlet></router-outlet>
+  </div>
+</div>`
 })
 export class AppComponent {
   title = 'angular-material-api';
